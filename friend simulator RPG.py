37c040c80acce36cacd2af8d21bfb2179b96467c
josh5423 <36918976+josh5423@users.noreply.github.com>
2018-02-28 10:34:53 +0000

    Add files via upload

---
+++ friend simulator RPG.py
@@ -0,0 +1,423 @@
+# to put text at top
+print("")
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+
+print("")
+hp = 20
+boss_hp = 200
+y = 'no' 
+# how you start
+ans = 2
+print("")
+print("hello welcome to friend simulator")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+print("hello what is your name")
+#first input
+name = input ("my name is ")
+print("my name is barac")
+print(" what is your faviorte thing to do " + name)
+hobbie = input (" my faviourte thing to do is ")
+print ("I HATE " + hobbie)
+print("I can no longer talk to you good day good person")
+print("barac left the game")
+whatever = input ("please insert reaction here: ")
+print("George joined the game")
+print("to see george add this calulation 1+1")
+x = int(input(': '))
+if x == 2:
+    print(" george left the game")
+    print("...")
+    print("...")
+    print("...")
+    print("...")
+    print("blade joined the game")
+    print("to see blade ans this caculation")
+    print("452 x 333 x 200")
+    #CHANGES YOUR PATH 
+    y = input (":")
+    if y == 'no':
+        print("you just have to ans a simple maths quetion come on")
+        print("452 x 333 x 200")
+        y = input(":")
+        if y == 'no':
+            print("I DID NOT WANT TO DO THIS BUT YOU LEAVE ME NO CHOISE")
+            print("you will die now")
+            print("turn based RPG mode enitiated")
+            print("your speed is 20")
+            print("your Speical Attack is 20")
+            print("your attack is 5")
+            print("type = water")
+            print("your defence is 6")
+            print("your Speical defence is 20")
+            print("your name is " + name)
+            ready = input (":")
+            print("your turn")
+            print("options are attack, bag, flee")
+            options = input (":")
+            if options == 'bag':
+                print("nothing is in your bag because you dont have a bag you, stupid person")
+                print("oh darn")
+                print("boss used death stair")
+                print("you died")
+                input ("")
+                exit()
+            if options == 'flee':
+                print("you can't run away from a trainer battle in another dimention")
+                print("oh darn")
+                print("boss used death stair")
+                print("you died")
+                input ("")
+                exit()
+            if options == 'attack':
+                print("your choices are water gun or bubble")
+                attack = input(":")
+                if attack == 'water gun':
+                    print("it was not very effective")
+                    boss_hp = 18
+                    print("boss used death stair")
+                    print("it was super effective")
+                    hp = '0'
+                    if hp == '0':
+                        print("you died")
+                        money = input ("pay him $3.50")
+                        if money == '$3.50':
+                            print("you died")
+                if attack =='bubble':
+                    print("it did 0 damage")
+                    print("boss said you found my weekness bubbles")
+                    input (":")
+                    print("boss used death stair")
+                    print("you died")
+                    print("ok")
+                    print("you paid boss £300000000000000")
+                    print("you died")
+                    input (":")
+            if options == 'pokemon' or 'Pokemon':
+                print("choose " + name)
+                print("george")
+                print("or charazard")
+                pokemon = input (": ")
+                if pokemon == name:
+                    print("boss used death stair")
+                    print("you paid boss £$£$331359397982")
+                    print("you died")
+                    input
+                if pokemon == 'george':
+                    print("go george")
+                    print("you have 4 options")
+                    print("attack bag flee or Pokemon")
+                    options_2 = input (": ")
+                    if options_2 == 'attack':
+                        print("your attack choises are uganda knuckles, meme blast or ...")
+                        attack_2 = input (": ")
+                        if attack_2 == 'uganda knuckles':
+                            print("do you know da way")
+                            print("you died")
+                        if attack_2 == 'meme blast':
+                            print("you used meme blast")
+                            print("you put a meme youtube compilation video on")
+                            print("boss used death stair")
+                            print("you and your memes died")
+                            input("")
+                            print("credits go to joshua-paul Rowe")
+                            input (":")
+                            exit()
+                        if attack_2 == '...':
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("boss: are you going to have your turn")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("boss I don't like awkward silence") 
+                            print("boss is confused")
+                            print("...")
+                            print("boss used death stair")
+                            print("boss got confused and damaged itself")
+                            print("boss died")
+                            print("thank you for playing sequal coming soon")
+                            input(":")
+                            exit()
+                    if options_2 == 'bag':
+                        print("george has a bag")
+                        print("george took out computer")
+                        print("george is watching barac")
+                        print("barac is green")
+                        print("barac is near a child")
+                        print("the child has had red liquid poor out of her")
+                        print("the child has fallen to the floor")
+                        print("george laughs")
+                        print("barac points at george")
+                        print("george faints")
+                        print("death")
+                        input("")
+                    if options_2 == 'flee':
+                        print("george ran")
+                        print("uganda knukles: do you know da way")
+                        print("you died")
+                    if options_2 == 'pokemon':
+                        print("all pokemon fainted")
+                        print("boss used death stair")
+                        print("you died")
+                        exit("")
+                if pokemon == 'charazard':
+                    print("you have 4 options")
+                    print("attack bag flee pokemon")
+                    options_3 = input (":")
+                    if options_3 == 'attack':
+                        print("flame thrower, fly,")
+                        attack_3 = input ("")
+                        if attack_3 == 'flame thrower':
+                            print("it is not very effective")
+                            print("boss fainted you win")
+                            print("charazard fainted")
+                            print("you are now stuck in this dimention")
+                            print("you died")
+                            print("...")
+                            input("")
+                            exit()
+                        if attack_3 == 'fly':
+                            print("fly failed")
+                            print("boss used death stair")
+                            print("you died")
+                            input("")
+                            exit()
+                    if options_3 == 'bag':
+                        print("you dont have a bag")
+                        print("boss used death stair")
+                        print("you died")
+                        exit()
+                    if options_3 == 'flee':
+                        print("you can't flee")
+                        print("boss used death stair")
+                        print("you died")
+                        exit()
+                    if options_3 == 'pokemon':
+                        print("choose " + name)
+                print("george")
+                print("or charazard")
+                pokemon = input (": ")
+                if pokemon == name:
+                    print("boss used death stair")
+                    print("you paid boss £$£$331359397982")
+                    print("you died")
+                    input
+                if pokemon == 'george':
+                    print("go george")
+                    print("you have 4 options")
+                    print("attack bag flee or Pokemon")
+                    options_2 = input (": ")
+                    if options_2 == 'attack':
+                        print("your attack choises are uganda knuckles, meme blast or ...")
+                        attack_2 = input (": ")
+                        if attack_2 == 'uganda knuckles':
+                            print("do you know da way")
+                            print("you died")
+                        if attack_2 == 'meme blast':
+                            print("you used meme blast")
+                            print("you put a meme youtube compilation video on")
+                            print("boss used death stair")
+                            print("you and your memes died")
+                            input("")
+                            print("credits go to joshua-paul Rowe")
+                            input (":")
+                            exit()
+                        if attack_2 == '...':
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("boss: are you going to have your turn")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("...")
+                            print("boss I don't like awkward silence") 
+                            print("boss is confused")
+                            print("...")
+                            print("boss used death stair")
+                            print("boss got confused and damaged itself")
+                            print("boss died")
+                            print("thank you for playing sequal coming soon")
+                            input(":")
+                            exit()
+                    if options_2 == 'bag':
+                        print("george has a bag")
+                        print("george took out computer")
+                        print("george is watching barac")
+                        print("barac is green")
+                        print("barac is near a child")
+                        print("the child has had red liquid poor out of her")
+                        print("the child has fallen to the floor")
+                        print("george laughs")
+                        print("barac points at george")
+                        print("george faints")
+                        print("death")
+                        input("")
+                    if options_2 == 'flee':
+                        print("george ran")
+                        print("uganda knukles: do you know da way")
+                        print("you died")
+                    if options_2 == 'pokemon':
+                        print("all pokemon fainted")
+                        print("boss used death stair")
+                        print("you died")
+                        exit()
+                    
+                    
+                        
